5c0e92e91013801de7877c74c9984041c5050e0d
Sharad <sharad@localhost.localdomain>
2020-01-03 11:45:14 -0800

    PythonCode

---
+++ pythoncode.py
@@ -0,0 +1,11 @@
+def filter_odd_even(l):
+    odd=[]
+    even=[]
+    for i in l:
+        if i%2==0:
+            even.append(i)
+        else: odd.append(i)
+    output = [odd , even]
+    return  output
+num=[1,2,3,4,5,6,7,8]
+print(filter_odd_even(num))
